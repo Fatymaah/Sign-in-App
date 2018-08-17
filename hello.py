ffc7e36c6e5e31be3c30ee9cd590c2a4cc5290c4
Fatymaah <fatymahmohamed@gmail.com>
2018-08-17 14:53:55 +0300

    path

--- hello.py
+++ hello.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello There !</h1>' 
 
+@app.route('/home')
+def home():
+    return '<h1> You are on the homepage !</h1>' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
