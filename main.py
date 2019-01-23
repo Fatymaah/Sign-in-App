ec2fedd8b02e18e4880037438c3d17072f46bd38
Fatma Mohamed <35401881+Fatymaah@users.noreply.github.com>
2019-01-23 12:47:24 +0300

    update

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def process():
 @app.route('/home', methods=['GET', 'POST'])
 def home():
 	links = ['https://www.youtube.com', 'https://www.bing.com', 'https://www.python.org', 'https://www.enkato.com']
-	return render_template('example.html', links=links)
+	return render_template('index.html', links=links)
 
 if __name__ == '__main__':
 	app.run(debug=True)
